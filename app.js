b6c6b702082338ae1881854aa41bb680d0c87cbf
Jeremy Hastings <jeremy.hastings@mac.com>
2020-02-15 16:27:21 +0000

    Events and Event Handling: Rolling the Dice Completed.

--- app.js
+++ app.js
@@ -11,23 +11,43 @@ GAME RULES:
 
 // Game Variables
 
-var activePlayer, dice, roundScore, scores;
+var activePlayer, roundScore, scores;
 
 activePlayer = 0;
-dice = Math.floor(Math.random() * 6) + 1;
 roundScore = 0;
 scores = [0, 0];
 
 // Hide Dice at start of game.
 document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// Zero out scores
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 
+// https://developer.mozilla.org/en-US/docs/Web/Events
+document.querySelector('.btn-roll').addEventListener('click', function() { // Using anonymous function instead of calling one.
+    
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result.
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice +'.png';
+
+    // 3. Update the round score IF the rolled number is not 1.
+
+
+
+}); // Callback function, function called by a function.
 
 
 // DICE /////////////
 // console.log(dice);
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
